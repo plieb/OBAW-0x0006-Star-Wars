952ecce74c08087aa9904338a88618eae09a4b34
PE Lieb <lieb.pe@gmail.com>
2017-02-13 00:07:55 +0100

    [FIX] JSON body

--- src/actions/get-info-people.js
+++ src/actions/get-info-people.js
@@ -12,7 +12,8 @@ export default async function getInfoPeople (res) {
   console.log(people)
   console.log('======================================')
   replies.push(formatter.formatMsg(`Looking for information regarding ${people.value}`))
-  const peopleAnswer = await agent('GET', `https://swapi.co/api/people/?search=${people.value}`)
+  const response = await agent('GET', `https://swapi.co/api/people/?search=${people.value}`)
+  const peopleAnswer = response.body
   console.log('======================================')
   console.log(peopleAnswer)
   console.log(peopleAnswer.results[0])
