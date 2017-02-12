c4023d519c8bfde0b5aa20ef80e35ff3749de696
PE Lieb <lieb.pe@gmail.com>
2017-02-12 23:32:58 +0100

    [FIX] formatter was using missing packages

--- src/formatter.js
+++ src/formatter.js
@@ -1,6 +1,4 @@
 /* module imports */
-const moment = require('moment')
-const numeral = require('numeral')
 
 exports.formatMsg = msg => ({
   type: 'text',
@@ -12,7 +10,7 @@ exports.formatProperties = (properties) => {
   properties.forEach((property) => {
     elements.push({
       title: property.get('Title__c'),
-      subtitle: `${property.get('Address__c')}, ${property.get('City__c')} ${property.get('State__c')} · ${numeral(property.get('Price__c')).format('$0,0')}`,
+      subtitle: `${property.get('Address__c')}, ${property.get('City__c')} ${property.get('State__c')} · ${(property.get('Price__c')).format('$0,0')}`,
       imageUrl: property.get('Picture__c'),
       buttons: [
         {
@@ -54,7 +52,6 @@ exports.formatPriceChanges = (priceChanges) => {
     const property = priceChange.get('Parent')
     elements.push({
       title: `${property.Address__c}, ${property.City__c} ${property.State__c}`,
-      subtitle: `Old price: ${numeral(priceChange.get('OldValue')).format('$0,0')} · New price: ${numeral(priceChange.get('NewValue')).format('$0,0')} on ${moment(priceChange.get('CreatedDate')).format('MMM Do')}`,
       imageUrl: property.Picture__c,
       buttons: [
         {
@@ -87,55 +84,6 @@ exports.formatPriceChanges = (priceChanges) => {
   return {
     type: 'carouselle',
     content: elements,
-  }
-}
-
-
-exports.formatAppointment = (property) => {
-  const options = [
-    `${moment().add(1, 'days').format('dddd MMM Do')} at 10am`,
-    `${moment().add(2, 'days').format('dddd MMM Do')} at 9am`,
-    `${moment().add(2, 'days').format('dddd MMM Do')} at 5pm`,
-    `${moment().add(3, 'days').format('dddd MMM Do')} at 1pm`,
-    `${moment().add(3, 'days').format('dddd MMM Do')} at 6pm`,
-  ]
-  return {
-    type: 'card',
-    content: {
-      title: `Select one of the available appointments below at ${property.get('Address__c')} in ${property.get('City__c')}.`,
-      buttons: [
-        {
-          type: 'postback',
-          title: options[0],
-          value: JSON.stringify({
-            text: `Confirm visit, ${property.get('Address__c')} in ${property.get('City__c')}, ${options[0]}`,
-            propertyId: property.getId(),
-            date: options[0],
-            city: property.get('City__c'),
-          }),
-        },
-        {
-          type: 'postback',
-          title: options[1],
-          value: JSON.stringify({
-            text: `Confirm visit, ${property.get('Address__c')} in ${property.get('City__c')}, ${options[1]}`,
-            propertyId: property.getId(),
-            date: options[1],
-            city: property.get('City__c'),
-          }),
-        },
-        {
-          type: 'postback',
-          title: options[2],
-          value: JSON.stringify({
-            text: `Confirm visit, ${property.get('Address__c')} in ${property.get('City__c')}, ${options[2]}`,
-            propertyId: property.getId(),
-            date: options[2],
-            city: property.get('City__c'),
-          }),
-        },
-      ],
-    },
   }
 }
 
