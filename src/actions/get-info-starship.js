bdb3ae3ea6d182ed27643749e449ceb9b3c3eb6d
PE Lieb <lieb.pe@gmail.com>
2017-02-12 23:37:23 +0100

    [ADD] missing files

---
+++ src/actions/get-info-starship.js
@@ -0,0 +1,9 @@
+/* module imports */
+const formatter = require('../formatter')
+
+export default async function getInfoPeople (res) {
+  console.log('GET INFO PEOPLE')
+
+  const replies = []
+  return replies
+}
