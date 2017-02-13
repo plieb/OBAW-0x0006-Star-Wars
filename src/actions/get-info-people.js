ab2f2729203d207a2a46d6a031a4f354d0c72be4
PE Lieb <lieb.pe@gmail.com>
2017-02-13 13:52:29 +0100

    [UPD] log starships

--- src/actions/get-info-people.js
+++ src/actions/get-info-people.js
@@ -22,13 +22,18 @@ export default async function getInfoPeople (res) {
     const eyeColor = peopleAnswer.results[0].eye_color
     const birthYear = peopleAnswer.results[0].birth_year
     const gender = peopleAnswer.results[0].gender
-    const info = `${people.value} :\n- height: ${height}\n- mass: ${mass}\n- hair color: ${hairColor}\n- skin color: ${skinColor}\n- eye color: ${eyeColor}\n- birth year: ${birthYear}\n- gender: ${gender}\n\n See below for cmplementary information.`
+    const info = `${people.value} :\n- height: ${height}\n- mass: ${mass}\n- hair color: ${hairColor}\n- skin color: ${skinColor}\n- eye color: ${eyeColor}\n- birth year: ${birthYear}\n- gender: ${gender}`
     replies.push(formatter.formatMsg(info))
     if (peopleAnswer.results[0].homeworld) {
       const responsePlanet = await agent('GET', peopleAnswer.results[0].homeworld)
       const planetAnswer = responsePlanet.body
       quickReplies.push({ name: planetAnswer.name, value: `Can I get information about ${planetAnswer.name}` })
     }
+    console.log('======================================')
+    console.log(peopleAnswer.results[0].starships.length)
+    console.log(peopleAnswer.results[0].starships)
+    console.log(peopleAnswer.results[0].starships[0])
+    console.log('======================================')
     if (peopleAnswer.results[0].starships.length) {
       const responseStarship = await agent('GET', peopleAnswer.results[0].starships[0])
       const starshipAnswer = responseStarship.body
