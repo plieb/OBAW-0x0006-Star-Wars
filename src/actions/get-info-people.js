9e33b868b377dc81a4ef8ddee8a14c803f8bc50c
PE Lieb <lieb.pe@gmail.com>
2017-02-13 13:15:53 +0100

    [FIX] url for homeworld planet

--- src/actions/get-info-people.js
+++ src/actions/get-info-people.js
@@ -25,10 +25,16 @@ export default async function getInfoPeople (res) {
     const info = `${people.value} :\n- height: ${height}\n- mass: ${mass}\n- hair color: ${hairColor}\n- skin color: ${skinColor}\n- eye color: ${eyeColor}\n- birth year: ${birthYear}\n- gender: ${gender}`
     replies.push(formatter.formatMsg(info))
     if (peopleAnswer.homeworld) {
-      const responsePlanet = await agent('GET', `https://swapi.co/api/people/?search=${people.value}`)
+      const responsePlanet = await agent('GET', peopleAnswer.homeworld)
+      console.log('======================================')
+      console.log(responsePlanet)
+      console.log('======================================')
       const planetAnswer = responsePlanet.body
       quickReplies.push({ name: planetAnswer.name, value: `Can I get information about ${planetAnswer.name}` })
-      replies.push(formatter.quickReplies(quickReplies))
+      console.log('======================================')
+      console.log(quickReplies)
+      console.log('======================================')
+      replies.push(formatter.formatQuickReplies(quickReplies))
     }
   } else {
     replies.push(formatter.formatMsg(`Sorry I couldn't find any information regarding ${people.value}`))
